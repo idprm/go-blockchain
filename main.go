acac5b30ba5f9073a7595a46273964c7aa365ca3
Indra <indra@ide.web.id>
2022-07-07 12:14:58 +0700

    add chain on blockchain module

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/idprm/go-blockchain/wallet"
+)
+
+func init() {
+	log.SetPrefix("Blockchain: ")
+}
+
+func main() {
+	w := wallet.NewWallet()
+	fmt.Println(w.PrivateKeyStr())
+	fmt.Println(w.PublicKeyStr())
+	fmt.Println(w.BlockchainAddress())
+
+	t := wallet.NewTransaction(w.PrivateKey(), w.PublicKey(), w.BlockchainAddress(), "B", 1.0)
+	fmt.Printf("signature %s\n", t.GenerateSignature())
+}
